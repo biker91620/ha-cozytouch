a068970757db8262c19389ad88f7308b064d3f15
cyr-ius <cyr-ius@ipocus.net>
2020-05-22 10:48:16 +0200

    Add contact sensor

--- custom_components/cozytouch/binary_sensor.py
+++ custom_components/cozytouch/binary_sensor.py
@@ -4,7 +4,11 @@ import logging
 from cozytouchpy import CozytouchException
 from cozytouchpy.constant import DeviceType
 
-from homeassistant.components.binary_sensor import BinarySensorEntity
+from homeassistant.components.binary_sensor import (
+    BinarySensorEntity,
+    DEVICE_CLASS_WINDOW,
+    DEVICE_CLASS_OCCUPANCY,
+)
 
 from .const import COZYTOUCH_DATAS, DOMAIN
 
@@ -21,6 +25,10 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
             sensor for sensor in heater.sensors if sensor.widget == DeviceType.OCCUPANCY
         ]:
             devices.append(CozytouchOccupancySensor(sensor, heater))
+        for sensor in [
+            sensor for sensor in heater.sensors if sensor.widget == DeviceType.CONTACT
+        ]:
+            devices.append(CozytouchContactSensor(sensor, heater))
 
     _LOGGER.info("Found {count} binary sensor".format(count=len(devices)))
     async_add_entities(devices, True)
@@ -56,7 +64,58 @@ class CozytouchOccupancySensor(BinarySensorEntity):
     @property
     def device_class(self):
         """Return the device class."""
-        return "presence"
+        return DEVICE_CLASS_OCCUPANCY
+
+    async def async_update(self):
+        """Fetch new state data for this sensor."""
+        _LOGGER.debug("Update binary sensor {name}".format(name=self.name))
+        try:
+            await self.sensor.update()
+        except CozytouchException:
+            _LOGGER.error("Device data no retrieve {}".format(self.name))
+
+    @property
+    def device_info(self):
+        """Return the device info."""
+        return {
+            "name": self.ref_name,
+            "identifiers": {(DOMAIN, self.ref_id)},
+            "manufacturer": self.ref_manufacturer,
+            "via_device": (DOMAIN, self.sensor.data["placeOID"]),
+        }
+
+
+class CozytouchContactSensor(BinarySensorEntity):
+    """Occupancy sensor (present/not present)."""
+
+    def __init__(self, sensor, device):
+        """Initialize contact sensor."""
+        self.sensor = sensor
+        self.ref_id = device.id
+        self.ref_name = device.name
+        self.ref_manufacturer = device.manufacturer
+
+    @property
+    def unique_id(self):
+        """Return the unique id of this switch."""
+        return self.sensor.id
+
+    @property
+    def name(self):
+        """Return the display name of this switch."""
+        return "{place} {sensor}".format(
+            place=self.sensor.place.name, sensor=self.sensor.name
+        )
+
+    @property
+    def is_on(self):
+        """Return true if contact is opened."""
+        return self.sensor.is_opened
+
+    @property
+    def device_class(self):
+        """Return the device class."""
+        return DEVICE_CLASS_WINDOW
 
     async def async_update(self):
         """Fetch new state data for this sensor."""
